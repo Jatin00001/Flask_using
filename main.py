9ea79127a69464c579b097af4c63a2f24ecb9cbe
Jatin <datajatin90@gmail.com>
2023-06-11 22:08:11 +0530

    all done

--- main.py
+++ main.py
@@ -47,8 +47,24 @@ class Posts(db.Model):
     img_file = db.Column(db.String(12), nullable=True)
 @app.route("/")
 def home_route():
-    posts = Posts.query.filter_by().all()[0:parameter['no_of_post']]
-    return render_template('index.html', parameter = parameter, posts=posts)
+    posts = Posts.query.filter_by().all()
+    last = math.ceil(len(posts)/int(parameter['no_of_post']))
+    page = request.args.get('page')
+    if (not str(page).isnumeric()):
+        page = 1
+    page = int(page)
+    posts = posts[(page - 1)*int(parameter['no_of_post']): (page - 1) * int(parameter['no_of_post']) + int(parameter['no_of_post'])]
+    if(page == 1):
+        prev = "#"
+        next = "/?page=" + str(page + 1)
+    elif(page == last):
+        prev = "/?page=" + str(page - 1)
+        next = "#"
+    else:
+        prev = "/?page=" + str(page - 1)
+        next = "/?page=" + str(page + 1)
+
+    return render_template('index.html', parameter = parameter, posts=posts, prev=prev, next=next)
 
 @app.route("/contact",  methods = ['GET', 'POST'])
 def contact():
